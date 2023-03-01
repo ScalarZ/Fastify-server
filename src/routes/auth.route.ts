f023118c966737cd221af3fdfe94b0bb27bac994
ScalarZ <faresrahali44@gmail.com>
2023-03-01 14:50:06 +0100

    adding credentials

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,21 +1,34 @@
 import { FastifyInstance } from "fastify";
+import { send } from "process";
 
 export async function authRoute(fastify: FastifyInstance) {
-  fastify.get("/api/login", async (req, rep) => {
-    try {
-      const access_token = await rep.jwtSign({ name: "foo" });
-      rep.setCookie("access_token", access_token, {
-        path: "/",
-        secure: true,
-        sameSite: true,
-      });
-      return {
-        statusCode: 200,
-        message: "logged in",
-        cookie: `access_token=${access_token}; Path=/; Secure; SameSite=Strict`,
-      };
-    } catch (err) {
-      return err;
+  fastify.post("/api/login", async (req, rep) => {
+    if (!req.body)
+      rep.status(401).send({ statusCode: 401, message: "invalid credentials" });
+    const { username, password } = req.body as {
+      username: string;
+      password: string;
+    };
+    if (username === "admin" && password === "admin")
+      try {
+        const access_token = await rep.jwtSign({ username, type: "admin" });
+        rep.setCookie("access_token", access_token, {
+          path: "/",
+          secure: true,
+          sameSite: true,
+        });
+        return {
+          statusCode: 200,
+          message: "logged in",
+          cookie: `access_token=${access_token}; Path=/; Secure; SameSite=Strict`,
+        };
+      } catch (err) {
+        rep
+          .status(401)
+          .send({ statusCode: 503, message: "internal server error" });
+      }
+    else {
+      rep.status(401).send({ statusCode: 401, message: "invalid credentials" });
     }
   });
 
